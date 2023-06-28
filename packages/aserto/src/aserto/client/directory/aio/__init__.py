339265a36c0fac5550a1a2e1a2e71fddefa6b573
Ananya Kaushik <ananya@aserto.com>
2023-06-28 00:11:26 -0700

    async object wrappers

--- packages/aserto/src/aserto/client/directory/aio/__init__.py
+++ packages/aserto/src/aserto/client/directory/aio/__init__.py
@@ -1,12 +1,28 @@
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple, TypedDict
 
 import grpc.aio as grpc
-from aserto.directory.common.v2 import ObjectTypeIdentifier, PaginationRequest
+from aserto.directory.common.v2 import (
+    Object,
+    ObjectIdentifier,
+    ObjectTypeIdentifier,
+    PaginationRequest,
+    PaginationResponse,
+)
 from aserto.directory.exporter.v2 import ExporterStub
 from aserto.directory.importer.v2 import ImporterStub
-from aserto.directory.reader.v2 import GetObjectsRequest, GetObjectsResponse, ReaderStub
-from aserto.directory.writer.v2 import WriterStub
-from grpc import Channel, ChannelCredentials, ssl_channel_credentials
+from aserto.directory.reader.v2 import (
+    GetObjectManyRequest,
+    GetObjectRequest,
+    GetObjectsRequest,
+    GetObjectsResponse,
+    ReaderStub,
+)
+from aserto.directory.writer.v2 import DeleteObjectRequest, SetObjectRequest, WriterStub
+from grpc import ChannelCredentials, StatusCode, ssl_channel_credentials
+
+
+class NotFoundError(Exception):
+    pass
 
 
 class Directory:
@@ -54,6 +70,105 @@ class Directory:
             metadata=self._metadata,
         )
         return response
+
+    async def get_many_objects(
+        self,
+        objects: Optional[List[TypedDict("ObjectParams", {"key": str, "type": str})]] = None,
+    ) -> List[Object]:
+        """Retrieve a list of directory object using a list of object key and type pairs.
+        Returns a list of each objects, if an object with the specified key and type exists.
+
+        Parameters
+        ----
+        objects : list( dict(key: str, type: str) )
+            list of object key and object type pairs
+
+        Returns
+        ----
+        list
+            list of directory objects
+        """
+
+        identifiers = [ObjectIdentifier(key=x["key"], type=x["type"]) for x in objects]
+        print("identifiers", identifiers)
+        response = await self.reader.GetObjectMany(
+            GetObjectManyRequest(param=identifiers),
+            metadata=self._metadata,
+        )
+        return response.results
+
+    async def get_object(self, key: str, type: str) -> Object:
+        """Retrieve a directory object by its key and type.
+        Returns the object or raises a NotFoundError if an object with the
+        specified key and type doesn't exist.
+
+        Parameters
+        ----
+        key : str
+            an object key
+        type : str
+            an object type
+
+        Returns
+        ----
+        a directory object
+        """
+
+        try:
+            identifier = ObjectIdentifier(type=type, key=key)
+            response = await self.reader.GetObject(
+                GetObjectRequest(param=identifier), metadata=self._metadata
+            )
+            return response.result
+
+        except grpc.AioRpcError as err:
+            if err.code() == StatusCode.NOT_FOUND:
+                raise NotFoundError from err
+            raise
+
+    async def set_object(self, object: Object) -> Object:
+        """Updates a directory object given its key and type, or creates a new object.
+        Returns the created/updated object.
+
+        Parameters
+        ----
+        object : Object
+            key : str
+            type : str
+            display_name : str, optional
+            properties : dict, optional
+            hash : str (required, if updating existing object)
+
+        Returns
+        ----
+        a directory object
+        """
+
+        response = await self.writer.SetObject(
+            SetObjectRequest(object=object), metadata=self._metadata
+        )
+        return response.result
+
+    async def delete_object(self, key: str, type: str) -> None:
+        """Deletes a directory object given its key and type.
+        Returns None.
+
+        Parameters
+        ----
+        key : str
+            an object key
+        type : str
+            an object type
+
+        Returns
+        ----
+        None
+        """
+
+        identifier = ObjectIdentifier(type=type, key=key)
+        await self.writer.DeleteObject(
+            DeleteObjectRequest(param=identifier), metadata=self._metadata
+        )
 
     async def close(self) -> None:
         """Closes the gRPC channel"""
