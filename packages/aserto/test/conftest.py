0ba48afdde524a9cdf0a99c4b6e3d4061bdd8c10
Ananya Kaushik <ananya@aserto.com>
2023-07-10 17:37:18 -0700

    add fixtures and tests for get/set/delete object

--- packages/aserto/test/conftest.py
+++ packages/aserto/test/conftest.py
@@ -1,10 +1,13 @@
 import os.path
 import subprocess
+import uuid
 from dataclasses import dataclass
 from typing import Optional
 
 import grpc
 import pytest
+
+from aserto.client.directory import Directory, Object
 
 
 @dataclass(frozen=True)
@@ -63,6 +66,7 @@ def topaz_stop() -> None:
         check=True,
     )
 
+
 def topaz_wait_for_ready(svc: Service) -> None:
     channel = connect(svc)
     grpc.channel_ready_future(channel).result()
@@ -73,9 +77,40 @@ def connect(svc: Service) -> grpc.Channel:
         target=svc.address, credentials=grpc.ssl_channel_credentials(read_cert(svc.ca_cert_path))
     )
 
+
 def read_cert(path: Optional[str]) -> Optional[bytes]:
     if path is None:
         return None
 
     with open(path, "rb") as f:
         return f.read()
+
+
+@pytest.fixture
+def directory_client(topaz):
+    directory = Directory.connect(
+        address=topaz.directory.address, ca_cert=topaz.directory.ca_cert_path
+    )
+
+    yield directory
+
+
+@pytest.fixture
+def directory_setup(topaz, directory_client):
+    directory = directory_client
+
+    key_1 = uuid.uuid4().hex
+    key_2 = uuid.uuid4().hex
+
+    obj_1 = directory.set_object(Object(key=key_1, type="user", display_name="test user"))
+    obj_2 = directory.set_object(Object(key=key_2, type="group", display_name="test group"))
+
+    relation = directory.set_relation(
+        relation={
+            "subject": {"key": obj_1.key, "type": obj_1.type},
+            "object": {"key": obj_2.key, "type": obj_2.type},
+            "relation": "member",
+        }
+    )
+
+    yield obj_1, obj_2, relation
--- packages/aserto/test/test_directory.py
+++ packages/aserto/test/test_directory.py
@@ -1,16 +1,36 @@
 import uuid
 
-from aserto.client.directory import Directory, Object
+from pytest import raises
+
+from aserto.client.directory import Directory, NotFoundError, Object
 
 
-def test_set_object(topaz):
-    directory = Directory.connect(address=topaz.directory.address, ca_cert=topaz.directory.ca_cert_path)
+def test_delete_object(topaz, directory_client, directory_setup):
+    with raises(NotFoundError):
+        directory = directory_client
+        obj_1, _, _ = directory_setup
+
+        directory.delete_object(key=obj_1.key, type=obj_1.type)
+        obj = directory.get_object(key=obj_1.key, type=obj_1.type)
+
+
+def test_get_object(topaz, directory_client, directory_setup):
+    directory = directory_client
+    obj_1, _, _ = directory_setup
+
+    obj = directory.get_object(key=obj_1.key, type=obj_1.type)
+    assert obj.key == obj_1.key
+    assert obj.type == obj_1.type
+    # assert obj.display_name == "test user"
+
+
+def test_set_object(topaz, directory_client):
+    directory = directory_client
 
     key = uuid.uuid4().hex
 
-    directory.set_object(Object(key=key, type="user", display_name="test user"))
+    obj = directory.set_object(Object(key=key, type="user", display_name="test user"))
 
-    obj = directory.get_object(key=key, type="user")
     assert obj.key == key
     assert obj.type == "user"
     assert obj.display_name == "test user"
