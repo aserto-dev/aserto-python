0c9d9082c11682155d55d410e6a6b7c22819813e
Ananya Kaushik <ananya@aserto.com>
2023-07-12 03:58:33 -0700

    async object wrapper tests

--- packages/aserto/test/test_async_directory.py
+++ packages/aserto/test/test_async_directory.py
@@ -2,7 +2,7 @@ import asyncio
 import uuid
 from dataclasses import dataclass
 
-import grpc
+import grpc.aio as grpc
 import pytest
 from google.protobuf.json_format import MessageToJson
 
@@ -169,6 +169,17 @@ async def directory_async(async_directory_client: Directory):
 
 
 @pytest.mark.asyncio
+async def test_delete_object(directory_async: SetupData):
+    await directory_async.client.delete_object(
+        key=directory_async.obj_1.key, type=directory_async.obj_1.type
+    )
+    with pytest.raises(NotFoundError):
+        await directory_async.client.get_object(
+            key=directory_async.obj_1.key, type=directory_async.obj_1.type
+        )
+
+
+@pytest.mark.asyncio
 async def test_get_object(directory_async: SetupData):
     obj = await directory_async.client.get_object(
         key=directory_async.obj_1.key, type=directory_async.obj_1.type
@@ -184,6 +195,77 @@ async def test_object_not_found(directory_async: SetupData):
     key = uuid.uuid4().hex
     with pytest.raises(NotFoundError):
         await directory_async.client.get_object(key=key, type="user")
+
+
+@pytest.mark.asyncio
+async def test_object_invalid_arg(directory_async: SetupData):
+    with pytest.raises(grpc.AioRpcError, match="object identifier invalid argument") as err:
+        await directory_async.client.get_object(key=directory_async.obj_1.key, type="")
+
+
+@pytest.mark.skip(reason="topaz directory doesn't filter on type")
+@pytest.mark.asyncio
+async def test_get_objects_by_type(directory_async: SetupData):
+    objs_response = await directory_async.client.get_objects(
+        object_type=directory_async.obj_1.type, page=PaginationRequest(size=10)
+    )
+    objs = objs_response.results
+
+    assert directory_async.obj_1 in objs
+    assert directory_async.obj_1.type == directory_async.obj_3.type
+    assert directory_async.obj_1.type != directory_async.obj_2.type
+    assert directory_async.obj_3 in objs
+    assert directory_async.obj_2 not in objs
+
+
+@pytest.mark.asyncio
+async def test_get_objects(directory_async: SetupData):
+    objs_response = await directory_async.client.get_objects(page=PaginationRequest(size=10))
+    objs = objs_response.results
+
+    assert directory_async.obj_1 in objs
+    assert directory_async.obj_2 in objs
+    assert directory_async.obj_3 in objs
+    assert len(objs) == 3
+
+
+@pytest.mark.asyncio
+async def test_get_objects_paging(directory_async: SetupData):
+    objs_response_1 = await directory_async.client.get_objects(page=PaginationRequest(size=2))
+    assert len(objs_response_1.results) == 2
+    assert objs_response_1.page.next_token
+
+    objs_response_2 = await directory_async.client.get_objects(
+        page=PaginationRequest(size=2, token=objs_response_1.page.next_token)
+    )
+    assert len(objs_response_2.results) == 1
+    assert not objs_response_2.page.next_token
+
+
+@pytest.mark.asyncio
+async def test_get_objects_many(directory_async: SetupData):
+    objs = await directory_async.client.get_objects_many(
+        objects=[
+            ObjectIdentifier(key=directory_async.obj_1.key, type=directory_async.obj_1.type),
+            ObjectIdentifier(key=directory_async.obj_2.key, type=directory_async.obj_2.type),
+        ]
+    )
+
+    assert directory_async.obj_1 in objs
+    assert directory_async.obj_2 in objs
+    assert len(objs) == 2
+
+
+@pytest.mark.asyncio
+async def test_set_object(directory_async: SetupData):
+    obj = await directory_async.client.get_object(
+        key=directory_async.obj_1.key, type=directory_async.obj_1.type
+    )
+    updated_obj = await directory_async.client.set_object(
+        Object(key=obj.type, type=obj.type, hash=obj.hash, display_name="changed user")
+    )
+
+    assert updated_obj.display_name == "changed user"
 
 
 @pytest.mark.asyncio
